f423bd80ae404fffa2ee04d6c5209d83d5171270
Nic Haynes <nic@nicinabox.com>
2013-08-01 18:05:57 -0500

    Add deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,12 @@
 set :application, "unraid-bootstrap"
-set :repository,  "set your repository location here"
+set :repository,  "git@code.nicinabox.com:unraid-bootstrap"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "98.226.202.159"                          # Your HTTP server, Apache/etc
+role :app, "98.226.202.159"                          # This may be the same as your `Web` server
+role :db,  "98.226.202.159", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
