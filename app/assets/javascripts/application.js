119b6bdf0f14a18863e0d24352980ffadf5cc2b5
Nic Haynes <nic@nicinabox.com>
2013-08-02 16:24:55 -0500

    Add resetful users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,17 @@
 $(function() {
   $('[data-toggle=tooltip]').tooltip();
 });
+
+$(document).on('click', '[data-method]', function(e) {
+  var confirmed = true;
+
+  if ($(this).data('method') === 'delete') {
+    confirmed = confirm("Really delete this " + $(this).data('model') + '?');
+  }
+
+  if (confirmed) {
+    $(this.form).find('[name=_method]').val($(this).data('method'));
+  } else {
+    e.preventDefault();
+  }
+});
