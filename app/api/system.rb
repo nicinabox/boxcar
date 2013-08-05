675f46d2e90e1dac565edd63bfca665a919c4ec5
Nic Haynes <nic@nicinabox.com>
2013-08-05 11:37:19 -0500

    Need when strip happpens

--- app/api/system.rb
+++ app/api/system.rb
@@ -51,23 +51,23 @@ class System
 
   private
   def proc_uptime
-   `cat /proc/uptime`.strip
+   `cat /proc/uptime`
   end
 
   def manufacturer
-    `dmidecode -q -t 2 | awk -F: '/Manufacturer:/ {print $2}'`.strip
+    `dmidecode -q -t 2 | awk -F: '/Manufacturer:/ {print $2}'`
   end
 
   def product
-    `dmidecode -q -t 2 | awk -F: '/Product Name:/ {print $2}'`.strip
+    `dmidecode -q -t 2 | awk -F: '/Product Name:/ {print $2}'`
   end
 
   def processor
-    `dmidecode -q -t 4 | awk -F: '/Version:/ {print $2}'`.strip
+    `dmidecode -q -t 4 | awk -F: '/Version:/ {print $2}'`
   end
 
   def processor_clock
-    `dmidecode -q -t 4 | awk -F: '/Current Speed:/ {print $2}'`.strip
+    `dmidecode -q -t 4 | awk -F: '/Current Speed:/ {print $2}'`
   end
 
   def processor_cache
@@ -75,26 +75,26 @@ class System
   end
 
   def memory_module
-    `dmidecode -q -t 17 | awk '/Size:/ {total+=$2;unit=$3} END {print total,unit}'`.strip
+    `dmidecode -q -t 17 | awk '/Size:/ {total+=$2;unit=$3} END {print total,unit}'`
   end
 
   def memory_capacity
-    `dmidecode -q -t 16 | awk -F: '/Maximum Capacity:/ {print $2}'`.strip
+    `dmidecode -q -t 16 | awk -F: '/Maximum Capacity:/ {print $2}'`
   end
 
   def connected_interface
-    `ifconfig -s | awk '$1~/[0-9]$/ {print $1}'`.strip
+    `ifconfig -s | awk '$1~/[0-9]$/ {print $1}'`
   end
 
   def interface_speed(interface)
     if interface == 'bond0'
-      `cat /proc/net/bonding/bond0 | grep 'Mode:' | cut -d: -f2`.strip
+      `cat /proc/net/bonding/bond0 | grep 'Mode:' | cut -d: -f2`
     else
-      `ethtool #{interface} | awk -F: '/Speed:/ {print $2}'`.strip
+      `ethtool #{interface} | awk -F: '/Speed:/ {print $2}'`
     end
   end
 
   def interface_duplex(interface)
-    `ethtool #{interface} | awk -F: '/Duplex:/ {print $2}'`.strip
+    `ethtool #{interface} | awk -F: '/Duplex:/ {print $2}'`
   end
 end
