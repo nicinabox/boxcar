42f191f894fb77d3d476bc829dbcbeb662691739
Nic Haynes <nic@nicinabox.com>
2013-08-06 01:09:44 -0500

    Add slapt-get to installer

--- install.sh
+++ install.sh
@@ -13,11 +13,35 @@ if [[ $install_boxcar != "y" ]]; then
   exit
 fi
 
+fragile_install () {
+  for i in "$@"
+  do
+    echo "Install $i"
+    wget -q http://slackware.cs.utah.edu/pub/slackware/slackware-13.37/slackware/d/$i
+    installpkg $i
+  done
+}
+
+slapt_install () {
+  for i in "$@"
+  do
+    echo "Install $i"
+    slapt-get -i $i
+  done
+}
+
 cd /boot/extra
+
+slapt_get=( 'libassuan-2.0.1-i486-1.txz'
+            'gpgme-1.3.0-i486-1.txz'
+            'slapt-get-0.10.2p-i386-1.tgz')
+
+echo "Installing slapt-get..."
+fragile_install ${slapt_get[@]}
+
 deps=('gcc-4.5.2-i486-2.txz'
       'git-1.7.4.4-i486-1.txz'
       'glibc-2.13-i486-4.txz'
-      'glibc-i18n-2.13-i486-4.txz'
       'glibc-solibs-2.11.1-i486-3.txz'
       'libelf-0.8.13-i486-2.txz'
       'libmpc-0.8.2-i486-2.txz'
@@ -27,19 +51,10 @@ deps=('gcc-4.5.2-i486-2.txz'
       'python-2.6.6-i486-1.txz'
       'ruby-1.9.3_p0-i486-1_vb.txz')
 
-echo "Downloading dependencies..."
-for i in "${deps[@]}"
-do
-   wget http://slackware.cs.utah.edu/pub/slackware/slackware-13.37/slackware/d/$i
-done
+echo "Fetching dependencies..."
+slapt_install ${deps[@]}
 
-for i in "${deps[@]}"
-do
-  echo "Installing $i"
-  installpkg $i
-done
-
-echo "Installing Boxcar..."
-echo "Importing existing configuration..."
-echo "Updating /boot/go to start Boxcar on boot..."
-echo "All done!"
+# echo "Installing Boxcar..."
+# echo "Importing existing configuration..."
+# echo "Updating /boot/go to start Boxcar on boot..."
+# echo "All done!"
