3b740bc2c5243f700bc41bca75f34508d6ed925c
Nic Haynes <nic@nicinabox.com>
2013-08-07 10:06:40 -0500

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -3,10 +3,10 @@
 # Install: bash <(curl -s http://localhost:8000/pack.sh)
 #
 
-HOST=10.0.39
+HOST=10.0.0.39
 
 cd && mkdir build
 wget -q http://$HOST:8000/boxcar.zip
 mv boxcar.zip build/
-cd build && unzip boxcar.zip
-makepkg -c y ../boxcar.txz && cd
+cd build && unzip boxcar.zip && rm boxcar.zip
+makepkg -c y ../boxcar.txz && cd && rm -rf build
