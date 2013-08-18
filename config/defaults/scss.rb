a6ec7890d331ecf9905c034c45ddacba46326e81
Nic Haynes <nic@nicinabox.com>
2013-08-18 16:41:40 -0500

    Fix compass load path

--- config/defaults/scss.rb
+++ config/defaults/scss.rb
@@ -1,5 +1,5 @@
 class Main
-  set :scss, { :load_paths => [ root('app/assets/stylesheets') ] }
+  set :scss, { :load_paths => [root('app/assets/stylesheets')] + Compass.sass_engine_options[:load_paths] }
   set :scss, self.scss.merge(:line_numbers => true, :debug_info => true, :always_check => true) if self.development?
   set :scss, self.scss.merge(:style => :compressed) if self.production?
 
