6e0dfa96dff4b419bfd4de689ddf343d26be4eeb
Nic Haynes <nic@nicinabox.com>
2013-08-19 00:03:50 -0500

    Update database connect

--- db/database.rb
+++ db/database.rb
@@ -1,4 +1,2 @@
-# Connect to database
-ENV['RACK_ENV'] ||= 'development'
 db = YAML.load(File.read("config/database.yml"))
-ActiveRecord::Base.establish_connection db[ENV['RACK_ENV']]
+ActiveRecord::Base.establish_connection(db[Main.environment.to_s])
