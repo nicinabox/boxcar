9bd79c338aad8d2ebb0086e170707cda7c162fc9
Nic Haynes <nic@nicinabox.com>
2013-08-23 20:16:56 -0500

    puts shouldn't be necessary

--- lib/boxcar/api/system.rb
+++ lib/boxcar/api/system.rb
@@ -6,13 +6,13 @@ class Boxcar::System
   class << self
     def reboot
       if system 'reboot'
-        puts "Sit tight. The system is going down for a reboot."
+        "Sit tight. The system is going down for a reboot."
       end
     end
 
     def shutdown
       if system 'shutdown now'
-        puts 'The system is shutting down.'
+        'The system is shutting down.'
       end
     end
   end
