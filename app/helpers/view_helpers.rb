8f34edfa597295ead49b8dae7044c6968fe4e740
Nic Haynes <nic@nicinabox.com>
2013-08-24 00:48:29 -0500

    Refactor api into helpers

--- app/helpers/view_helpers.rb
+++ app/helpers/view_helpers.rb
@@ -1,5 +1,7 @@
 class Main
   module ViewHelpers
+    include ::Boxcar::Helpers
+
     def active_for(path, position = :ends)
       path_match =  case position
                     when :ends
@@ -18,21 +20,6 @@ class Main
     def no_results(message)
       erb :'shared/_no_results',
           :locals => { :message => message }
-    end
-
-    PREFIX = %W(TB GB MB KB B).freeze
-    def humanize_size(s)
-      s = s.to_f
-      i = PREFIX.length - 1
-      while s > 512 && i > 0
-        i -= 1
-        s /= 1024
-      end
-      ((s > 9 || s.modulo(1) < 0.1 ? '%d' : '%.1f') % s) + ' ' + PREFIX[i]
-    end
-
-    def to_bytes(size)
-      size.to_i * 1024
     end
   end
 
--- lib/boxcar/api/core.rb
+++
@@ -1,9 +0,0 @@
-require 'boxcar/helpers'
-
-class Boxcar::Core
-  class << self
-    def unraid?
-      `uname -r`.include?('unRAID')
-    end
-  end
-end
