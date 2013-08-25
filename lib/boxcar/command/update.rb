9fe3a9d8217446b286a24318fef06e5be1083dbd
Nic Haynes <nic@nicinabox.com>
2013-08-25 14:28:17 -0500

    Add notification lines to update command

--- lib/boxcar/command/update.rb
+++ lib/boxcar/command/update.rb
@@ -8,16 +8,23 @@ class Boxcar::Command::Update < Boxcar::Command::Base
     host    = 'https://github.com/nicinabox/boxcar/archive/'
     dest    = 'usr/apps/boxcar'
 
-    # Pack
+    `boxcar server:stop`
+
+    # Fetch
+    puts "Fetching..."
     FileUtils.cd('/tmp')
     FileUtils.mkdir_p("build/#{dest}")
     `wget -q --no-check-certificate #{host}#{version}.zip`
+
+    # Pack
+    puts "Packing..."
     `unzip -q #{version}`
     FileUtils.mv("boxcar-#{version}/*", "build/#{dest}")
     FileUtils.mkdir("build/#{dest}/log")
     `makepkg -c y /boot/extra/boxcar-#{version}.txz`
 
     # Install
+    puts "Installing..."
     FileUtils.cd('build')
     `installpkg /boot/extra/boxcar-#{version}.txz >/dev/null`
     FileUtils.cd('/tmp')
@@ -26,8 +33,12 @@ class Boxcar::Command::Update < Boxcar::Command::Base
     `export RACK_ENV=production`
 
     # Bundle & migrate
+    puts "Finishing..."
     FileUtils.cd("/#{dest}")
     `bundle`
     `rake db:migrate RACK_ENV=production >/dev/null`
+
+    puts "Updated to #{version}!"
+    `boxcar server:start`
   end
 end
