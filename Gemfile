a779cfaf572cd451fff75abac4d16be3554dce9a
Nic Haynes <nic@nicinabox.com>
2013-08-30 22:06:07 -0500

    Add compass

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem 'jekyll', '~> 1.1.2'
+gem 'compass'
---
+++ config.rb
@@ -0,0 +1,11 @@
+# require 'compass-normalize'
+# require 'modular-scale'
+
+http_path = "/"
+css_dir = "assets/stylesheets"
+sass_dir = "sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/javascripts"
+
+# output_style = :compressed
+# line_comments = false
