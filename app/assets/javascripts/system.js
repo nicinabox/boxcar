83a363b2fd49f7eabf44b7deccf9243a8445b847
Nic Aitch <nic@nicinabox.com>
2013-09-01 15:53:33 -0500

    Add modals for post shutdown, reboot

--- app/assets/javascripts/system.js
+++ app/assets/javascripts/system.js
@@ -1,9 +1,15 @@
 $(document).on('click.confirmed', '#power-controls .dropdown-menu a', function(e) {
   e.preventDefault();
+  var $modal = $('#power-modal');
+      message = $(this).data('message');
 
   if (e.namespace === 'confirmed') {
     $.post($(this).attr('href'), {}, function(data, textStatus, xhr) {
-      console.log(data);
+      $modal.find('h3').html('The system is ' + message + '.');
+      $modal.modal({
+        backdrop: 'static',
+        keyboard: false
+      });
     });
   }
 });
