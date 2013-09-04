d23c55018cbb65d315b533b23f6cde08f53f4a51
Nic Aitch <nic@nicinabox.com>
2013-09-03 19:39:13 -0500

    Fix dangerous, warm disks count

--- app/routes/disks.rb
+++ app/routes/disks.rb
@@ -6,10 +6,13 @@ class Main
       @disks = ::Boxcar::Disk.all
       temps = @disks.collect { |d| d.temp }.compact
 
-      if temps.select { |t| t > danger_temp if t }.any?
-        flash.now[:danger] = "You have #{temps.count} hot #{pluralize_without_count(temps.count, 'disk')}!"
-      elsif temps.select { |t| t.between? warning_temp, danger_temp if t }.any?
-        flash.now[:warning] = "You have #{temps.count} warm #{pluralize_without_count(temps.count, 'disk')}."
+      dangerous_temps = temps.select { |t| t > danger_temp }
+      warning_temps   = temps.select { |t| t.between? warning_temp, danger_temp }
+
+      if dangerous_temps.any?
+        flash.now[:danger] = "You have #{dangerous_temps.count} hot #{pluralize_without_count(dangerous_temps.count, 'disk')}!"
+      elsif warning_temps.any?
+        flash.now[:warning] = "You have #{warning_temps.count} warm #{pluralize_without_count(warning_temps.count, 'disk')}."
       end
 
       erb :'disks/index'
