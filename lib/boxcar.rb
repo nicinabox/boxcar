20224a9724b9fe0dab02715c364ec4e70e86a920
Nic Aitch <nic@nicinabox.com>
2013-09-07 12:28:34 -0500

    Remove BOXCAR_ROOT env var

--- lib/boxcar.rb
+++ lib/boxcar.rb
@@ -1,7 +1,7 @@
 ENV['RACK_ENV'] ||= 'development'
-ENV['BOXCAR_ROOT'] = File.expand_path('..', File.dirname(__FILE__))
 
-$:.unshift *Dir[ENV['BOXCAR_ROOT']]
+root = File.expand_path('..', File.dirname(__FILE__))
+$:.unshift *Dir[root]
 
 require 'config/environment'
 require 'boxcar/version'
