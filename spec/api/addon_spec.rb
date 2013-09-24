7166d9f218bf73f4198d38996e3048d2725a9765
Nic Aitch <nic@nicinabox.com>
2013-09-24 09:02:07 -0500

    Update addon_spec

--- spec/api/addon_spec.rb
+++ spec/api/addon_spec.rb
@@ -1,15 +1,18 @@
 require 'boxcar/api/addon'
 
-describe Boxcar::Addon, '.all' do
-  it "returns all addons from API as JSON" do
-    all_addons = Boxcar::Addon.all
-    all_addons.should be_a_kind_of Array
+describe Boxcar::Addon do
+  describe ".all" do
+    it "returns all addons from API as JSON" do
+      all_addons = Boxcar::Addon.all
+      all_addons.should be_a_kind_of Array
+    end
   end
-end
 
-describe Boxcar::Addon, '.find' do
-  it "returns a single addon from API as JSON" do
-    addon = Boxcar::Addon.find('dummy-addon')
-    addon.should be_a_kind_of Hash
+  describe '.find' do
+    it "returns a single addon from API as JSON" do
+      addon = Boxcar::Addon.find('dummy-addon')
+      addon.should be_a_kind_of Hash
+    end
   end
+
 end
