dd4d99abd518ed71ca27bbd29a55e408af0cef07
Nic Aitch <nic@nicinabox.com>
2013-09-25 10:56:24 -0500

    Add packages spec

---
+++ spec/command/packages_spec.rb
@@ -0,0 +1,19 @@
+require 'boxcar/cli'
+require 'boxcar/command/packages'
+
+module Boxcar::Command
+  describe Packages do
+
+    describe "find" do
+      it "finds available package versions by name" do
+        stderr, stdout = execute('packages:find openssl')
+        stderr.should == ""
+        stdout.should == <<-STDOUT
+openssl (1.0.1c, 0.9.8r, 0.9.8q)
+STDOUT
+
+      end
+    end
+
+  end
+end
