71b0d961a79b6dd05bef171fe4b39a59b89fe308
Nic Aitch <nic@nicinabox.com>
2013-11-07 19:37:29 -0600

    Update startup script, post install

--- install/doinst.sh
+++ install/doinst.sh
@@ -4,3 +4,9 @@ ln -s /usr/apps/boxcar/bin/boxcar /usr/local/bin/boxcar
 ln -s /etc/gemrc /root/.gemrc
 
 echo "source /etc/boxcar" >> ~/.bashrc
+
+# TODO: This is a workaround for started event not being fired in unRAID
+startup_script=/usr/local/emhttp/plugins/boxcar/event/started
+if ! grep -Fxq "$startup_script" /boot/config/go; then
+  echo "$startup_script" >> /boot/config/go
+fi
